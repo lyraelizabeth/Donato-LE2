0b34a2e94507d57e32bdbe18146279a9dc904961
Lyra Elizebth F. Donato <lilydonato27@gmail.com>
2023-03-02 02:58:30 +0800

    DonatoLE2

---
+++ dbo.Users.data.sql
@@ -0,0 +1,3 @@
+﻿SET IDENTITY_INSERT [dbo].[Users] ON
+INSERT INTO [dbo].[Users] ([Id], [UserName], [FirstName], [LastName], [Password]) VALUES (1, N'lefdonato', N'Lyra Elizabeth', N'Donato', N'2019133727')
+SET IDENTITY_INSERT [dbo].[Users] OFF
